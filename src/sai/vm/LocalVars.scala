db8ec73bf3a6baa4743867113db60f764667e121
Oliver Haase <haase@htwg-konstanz.de>
2017-02-23 17:27:33 +0100

    Class LocalVars changed into a case class.

--- src/sai/vm/LocalVars.scala
+++ src/sai/vm/LocalVars.scala
@@ -1,25 +1,24 @@
 package sai.vm
 
-class LocalVars(maxLocals: Int, argObjects: Map[Int, ObjectNode]) {
-  val localVars: List[Slot] = build(maxLocals, argObjects)
+case class LocalVars(localVars: List[Slot]) {  
   
-  private def build(maxLocals: Int, argObjects: Map[Int, ObjectNode]): List[Slot] = {
+  def get(index: Int) = localVars(index)
+  
+  def set(index: Int, slot: Slot) = new LocalVars(localVars.updated(index, slot))  
+  
+}
+
+object LocalVars {
+  def apply(maxLocals: Int, argObjects: Map[Int, ObjectNode]): LocalVars = {
     val builder:  Array[Slot] = new Array(maxLocals)
+
     for ( index <- 0 until maxLocals )
       if ( argObjects.contains(index) )
         builder(index) = argObjects.get(index).orNull
       else 
         builder(index) = PrimitiveSlot
     
-    builder.toList
+    new LocalVars(builder.toList)
   }
-  
-  def get(index: Int) = localVars(index)
-  
-  def set(index: Int, slot: Slot) = localVars.updated(index, slot)
-  
-
-  
-  // TODO override equals and hashcode
-  
 }
+
