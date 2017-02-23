fec6f50dd71c6e352af18288d826791030290047
Oliver Haase <haase@htwg-konstanz.de>
2017-02-23 17:18:47 +0100

    Class State changed into a case class.

--- src/sai/vm/State.scala
+++ src/sai/vm/State.scala
@@ -2,12 +2,4 @@ package sai.vm
 
 
 
-class State(localVars: LocalVars, opStack: List[Slot]) {
-  
-
-  
-  
-  // TODO override equals and hashCode
-  
-  
-}
+case class State(localVars: LocalVars, opStack: List[Slot]) 
