2ec70aef581373329959ecc2bb8a5137aba24438
Oliver Haase <haase@htwg-konstanz.de>
2017-02-23 17:09:52 +0100

    Case class OpStack added.

---
+++ src/sai/vm/OpStack.scala
@@ -0,0 +1,27 @@
+package sai.vm
+
+case class OpStack(stack: List[Slot]) {
+  
+  def push(slot: Slot): OpStack = new OpStack(slot :: stack)
+  
+  def push(slot: Slot, n: Int): OpStack = 
+    if ( n > 0 ) 
+      push(slot).push(slot, n -1)
+    else
+      this
+      
+  def pop: OpStack = new OpStack(stack tail)
+  
+  def pop(n: Int): OpStack = 
+    if ( n > 0 )
+      pop.pop(n -1)
+    else 
+      this
+      
+  def top = stack head    
+   
+}
+
+object OpStack {
+  def apply = new OpStack(Nil)
+}
