5f58e6cc058ec461cf695b3a7ee7acf4cd82fc0a
Markus Heilig <markusheilig@gmx.net>
2018-05-02 09:47:06 +0200

    add more unit tests for Method class

--- src/test/scala/MethodTest.scala
+++ src/test/scala/MethodTest.scala
@@ -1,49 +1,52 @@
 import org.scalatest.FlatSpec
 import org.scalatest.Matchers
 import sai.bytecode.Clazz
+import sai.bytecode.Method
+import sai.bytecode.instruction.EntryPoint
+import sai.bytecode.instruction.ExitPoint
 import sai.vm.State
 import sai.vm.PrimitiveSlot
 
 
 class MethodTest extends FlatSpec with Matchers {
 
-  "A Method" should "return a summary state with no local variables if a static void method without parameters is called" in {
-    val clazz = new Clazz("misc.SimpleMath")
-    val method = clazz.method("calcNothing").get
+  val clazz = new Clazz("misc.SimpleMath")
+  val calcNothing: Method = clazz.method("calcNothing").get
+  val add: Method = clazz.method("add").get
+  val meaningOfLife: Method = clazz.method("meaningOfLife").get
 
-    val states = method.summary
+  "A Method" should "return a summary state with no local variables if a static void method without parameters is called" in {
+    val states = calcNothing.summary
     states.size shouldBe 1
-    val State(localVars, opStack, edges) = states.toList.head
+    val State(localVars, _, _) = states.toList.head
     localVars.localVars shouldBe empty
-    opStack.stack.size shouldBe 0
-    edges.edges shouldBe empty
   }
 
   it should "return a summary state with local variables" in {
-    val clazz = new Clazz("misc.SimpleMath")
-    val method = clazz.method("add").get
-
-    val states = method.summary
+    val states = add.summary
     states.size shouldBe 1
-    val State(localVars, opStack, edges) = states.toList.head
+    val State(localVars, _, _) = states.toList.head
     localVars.localVars.head.toString shouldBe "java.lang.Integer"
     localVars.localVars(1) shouldBe PrimitiveSlot
     localVars.localVars(2) shouldBe PrimitiveSlot
-    opStack.stack.size shouldBe 0
-    edges.edges shouldBe empty
   }
 
   it should "return a summary state with the reference variable 'this' if the method is non-static" in {
-    val clazz = new Clazz("misc.SimpleMath")
-    val method = clazz.method("meaningOfLife").get
-
-    val states = method.summary
+    val states = meaningOfLife.summary
     states.size shouldBe 1
-    val State(localVars, opStack, edges) = states.toList.head
+    val State(localVars, _, _) = states.toList.head
     localVars.localVars.size shouldBe 1
     localVars.localVars.head.toString shouldBe "misc.SimpleMath"
-    opStack.stack shouldBe empty
-    edges.edges shouldBe empty
+  }
+
+  it should "give you the first instruction" in {
+    add.firstInstruction should not be null
+    add.firstInstruction should not be an [EntryPoint]
+  }
+
+  it should "have the a single ExitPoint as last instruction" in {
+    add.exitPoint shouldBe an [ExitPoint]
+    add.instructions.count(_.isInstanceOf[ExitPoint]) shouldBe 1
   }
 
 }
