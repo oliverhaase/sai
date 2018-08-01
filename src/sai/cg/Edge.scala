d814c056a4d2c60e8350c58134bfdded1b3d1e7f
Markus Heilig <markusheilig@gmx.net>
2018-08-01 08:46:22 +0200

    add syntactic sugar for creating edges (i.e. EdgeName(node1 -> node2) instead of EdgeName(node1, node2))

--- src/sai/cg/Edge.scala
+++ src/sai/cg/Edge.scala
@@ -3,21 +3,24 @@ package cg
 sealed trait Edge {
   val from: Node
   val to: Node
+
   override def toString: String = s"${getClass.getSimpleName}($from -> $to)"
 }
 
 case class PointsToEdge private(from: ReferenceNode, to: ObjectNode) extends Edge
+
 case class DeferredEdge private(from: ReferenceNode, to: ReferenceNode) extends Edge
+
 case class FieldEdge private(from: ObjectNode, to: FieldReferenceNode) extends Edge
 
 object PointsToEdge {
-  def apply(tuple: (ReferenceNode, ObjectNode)): PointsToEdge = PointsToEdge(tuple._1, tuple._2)
+  def apply(fromTo: (ReferenceNode, ObjectNode)): PointsToEdge = PointsToEdge(fromTo._1, fromTo._2)
 }
 
 object DeferredEdge {
-  def apply(tuple: (ReferenceNode, ReferenceNode)): DeferredEdge = DeferredEdge(tuple._1, tuple._2)
+  def apply(fromTo: (ReferenceNode, ReferenceNode)): DeferredEdge = DeferredEdge(fromTo._1, fromTo._2)
 }
 
 object FieldEdge {
-  def apply(tuple: (ObjectNode, FieldReferenceNode)): FieldEdge = FieldEdge(tuple._1, tuple._2)
+  def apply(fromTo: (ObjectNode, FieldReferenceNode)): FieldEdge = FieldEdge(fromTo._1, fromTo._2)
 }
