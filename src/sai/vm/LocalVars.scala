afec377cf14373dc95d8b292ce26d34fb47c9fb6
Markus Heilig <markusheilig@gmx.net>
2018-08-06 14:59:05 +0200

    merge two local variable objects with each other

--- src/sai/vm/LocalVars.scala
+++ src/sai/vm/LocalVars.scala
@@ -1,24 +1,33 @@
 package sai.vm
 
-case class LocalVars(localVars: List[Slot]) {  
+import sai.vm.Reference.Null
+
+case class LocalVars(localVars: List[Slot]) {
 
   def get(index: Int) = localVars(index)
-  
-  def set(index: Int, slot: Slot) = new LocalVars(localVars.updated(index, slot))  
-  
+
+  def set(index: Int, slot: Slot) = new LocalVars(localVars.updated(index, slot))
+
+  def merge(other: LocalVars): LocalVars = {
+    val slots = for {
+      (mySlot, otherSlot) <- localVars.zipAll(other.localVars, MultivalueSlot(), MultivalueSlot())
+      merged = mySlot.merge(otherSlot)
+    } yield merged
+    LocalVars(slots)
+  }
+
 }
 
 object LocalVars {
   def apply(maxLocals: Int, argObjects: Map[Int, Reference]): LocalVars = {
-    val builder:  Array[Slot] = new Array(maxLocals)
+    val builder: Array[Slot] = new Array(maxLocals)
 
-    for ( index <- 0 until maxLocals )
-      if ( argObjects.contains(index) )
-        builder(index) = argObjects.get(index).orNull
-      else 
-        builder(index) = PrimitiveSlot
+    for (index <- 0 until maxLocals)
+      if (argObjects.contains(index))
+        builder(index) = argObjects.getOrElse(index, Null)
+      else
+        builder(index) = Null
 
     new LocalVars(builder.toList)
   }
 }
-
