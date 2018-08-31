9b4ea44f2008277911b3b474495123805b33846e
Markus Heilig <markusheilig@gmx.net>
2018-08-31 13:37:11 +0200

    check areturn for 'null' return value

--- src/sai/vm/interpreter/impl/AreturnInterpreter.scala
+++ src/sai/vm/interpreter/impl/AreturnInterpreter.scala
@@ -3,6 +3,7 @@ package vm.interpreter.impl
 import cg.{ArgEscape, PhantomReturnNode}
 import org.apache.bcel.generic.{ARETURN, ObjectType}
 import sai.vm.{ObjectRef, OpStack}
+import sai.vm.ObjectRef.Null
 import vm.Frame
 import vm.interpreter.InstructionInterpreter
 import vm.interpreter.InstructionInterpreter.Interpreter
@@ -12,12 +13,18 @@ private[interpreter] object AreturnInterpreter extends InstructionInterpreter[AR
     case frame@Frame(method, _, stack, _, cg) =>
       assert(i.getType.isInstanceOf[ObjectType])
       stack.peek match {
+        case Null =>
+          val returnNode = new PhantomReturnNode(method.id)
+          val updatedCG =
+            cg.addNodes(returnNode)
+              .updateEscapeState(returnNode -> ArgEscape)
+          frame.copy(stack = OpStack(), cg = updatedCG)
         case _@ObjectRef(_, node) =>
           val returnNode = new PhantomReturnNode(method.id)
           val updatedCG =
             cg.addNodes(returnNode)
-            .addEdge(returnNode -> node)
-            .updateEscapeState(returnNode -> ArgEscape)
+              .addEdge(returnNode -> node)
+              .updateEscapeState(returnNode -> ArgEscape)
           frame.copy(stack = OpStack(), cg = updatedCG)
       }
   }
