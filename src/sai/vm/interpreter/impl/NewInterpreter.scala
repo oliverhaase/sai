cc87b65da5e07ade5179c9aa93313feebcbb88ee
Markus Heilig <markusheilig@gmx.net>
2018-09-20 19:44:15 +0200

    use correct function ('addNode' instead of 'addNodes' for adding a single node to the CG)

--- src/sai/vm/interpreter/impl/NewInterpreter.scala
+++ src/sai/vm/interpreter/impl/NewInterpreter.scala
@@ -9,14 +9,14 @@ import vm.interpreter.{Helper, Id, InstructionInterpreter, InterpreterBuilder}
 private[interpreter] object NewInterpreter extends InterpreterBuilder[NEW] {
 
   override def apply(i: NEW): InstructionInterpreter = {
-    case frame @ Frame(method, cpg, stack, _, cg) =>
-      val objectNode    = ObjectNode(Id(method, i))
+    case frame@Frame(method, cpg, stack, _, cg) =>
+      val objectNode = ObjectNode(Id(method, i))
       val referenceType = i.getLoadClassType(cpg)
-      val escapeState   = Helper.determineEscapeState(referenceType)
+      val escapeState = Helper.determineEscapeState(referenceType)
       val updatedCG =
-        cg.addNodes(objectNode)
+        cg.addNode(objectNode)
           .updateEscapeState(objectNode -> escapeState)
-      val objectRef    = Reference(referenceType, objectNode)
+      val objectRef = Reference(referenceType, objectNode)
       val updatedStack = stack.push(objectRef)
       frame.copy(stack = updatedStack, cg = updatedCG)
   }
