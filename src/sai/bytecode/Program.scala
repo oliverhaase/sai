fb6c1df9c80bc6766339f21af6999b05acc76e4a
Markus Heilig <markusheilig@gmx.net>
2018-09-23 10:00:08 +0200

    use 'Program' object to reuse loaded classes

--- src/sai/bytecode/Program.scala
+++ src/sai/bytecode/Program.scala
@@ -1,5 +1,11 @@
 package sai.bytecode
 
 object Program {
-  var classes: List[Clazz] = Nil
+
+  private type ClassName = String
+
+  private val classes = scala.collection.mutable.Map.empty[ClassName, Clazz]
+
+  def getClass(name: ClassName) = classes.getOrElseUpdate(name, new Clazz(name))
+
 }
