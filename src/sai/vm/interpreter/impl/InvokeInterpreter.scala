011839d1191c009a2024010c4dee8805a0ff986f
Markus Heilig <markusheilig@gmx.net>
2018-10-08 20:42:46 +0200

    use InvokeInterpreter for all invoke instructions but 'invokedynamic'
    right now, lambdas are not supported

--- src/sai/vm/interpreter/impl/InvokeInterpreter.scala
+++ src/sai/vm/interpreter/impl/InvokeInterpreter.scala
@@ -1,11 +1,12 @@
 package vm.interpreter.impl
 
-import cg.ConnectionGraph
-import org.apache.bcel.generic.InvokeInstruction
+import cg.{ConnectionGraph, PhantomReturnNode}
+import org.apache.bcel.classfile.{ConstantInvokeDynamic, ConstantNameAndType}
+import org.apache.bcel.generic._
 import sai.bytecode.{Clazz, Method, Program}
-import sai.vm.{DontCare, OpStack}
-import vm.{Frame, IntraproceduralAnalysis}
+import sai.vm.{DontCare, Reference}
 import vm.interpreter.{InstructionInterpreter, InterpreterBuilder}
+import vm.{Frame, SummaryInformation}
 
 private[interpreter] object InvokeInterpreter extends InterpreterBuilder[InvokeInstruction] {
 
@@ -13,16 +14,115 @@ private[interpreter] object InvokeInterpreter extends InterpreterBuilder[InvokeI
     override protected[interpreter] def doInterpret(frame: Frame): Frame = {
       val Frame(_, cpg, stack, _, _, _) = frame
 
-      val updatedCG    = frame.cg.merge(calc(i, frame))
-      val updatedStack = stack.pop(i.consumeStack(cpg)).push(DontCare)
+      val clazzbane = i.getClassName(cpg)
+      val name = i.getMethodName(cpg)
+      val methodToInvoke = lookupMethodToInvoke(frame, i)
+
+      // calculate summary for 'method to invoke'
+      val resultSummary = calculateSummary(methodToInvoke, frame)
+      val updatedCG     = frame.cg.merge(resultSummary)
+
+      // fetch return value from method invocation
+      val returnValue = resultSummary.nodes.find(node =>
+        node.isInstanceOf[PhantomReturnNode] && node.id == methodToInvoke.id) match {
+        case Some(returnNode) =>
+          Some(Reference(i.getReturnType(cpg), returnNode))
+        case None =>
+          None
+      }
+
+      // push return value on stack (or fill up with DontCare if there is no return value)
+      val updatedStack = if (returnValue.isDefined) {
+        stack.pop(i.consumeStack(cpg)).push(returnValue.get)
+      } else {
+        stack.pop(i.consumeStack(cpg)).push(DontCare, i.produceStack(cpg))
+      }
+
       frame.copy(stack = updatedStack, cg = updatedCG)
     }
   }
 
-  def calc(i: InvokeInstruction, frame: Frame): ConnectionGraph = {
-    val Frame(currentMethod, cpg, _, _, _, summaryCache) = frame
+  /**
+    * Lookup "the real" method to invoke based on a generic invoke instruction 'i'.
+    * The JVMS defines different lookup strategies for invoke instructions.
+    */
+  private def lookupMethodToInvoke(frame: Frame, i: InvokeInstruction): Method = {
+    val pool = frame.cpg
 
-    val methodToInvoke = Program.getClass(i.getClassName(cpg)).method(i.getMethodName(cpg)).get
+    // invokespecial, invokeinterface, and invokevirtual are invoked with an object reference
+    // invokedynamic and invokestatic have no object reference
+    val objectRef = {
+      i match {
+        case _: INVOKEDYNAMIC =>
+          None
+        case _: INVOKESTATIC  =>
+          None
+        case _ =>
+          val updatedStack = frame.stack.pop(i.consumeStack(pool) - 1)
+          val ref =
+            updatedStack.peek
+              .asInstanceOf[Reference]
+              .referenceType
+              .asInstanceOf[ObjectType]
+          Some(ref.getClassName)
+      }
+    }
+
+    // check if a method matches the resolved method name & method signature
+    def matches(m: Method): Boolean = {
+      m.signature == i.getSignature(pool) && m.name == i.getMethodName(pool)
+    }
+
+    // lookup
+    i match {
+      case _: INVOKEDYNAMIC =>
+        throw new NotImplementedError("INVOKEDYNAMIC is not supported")
+      case _: INVOKEINTERFACE =>
+        val clazz      = Program.getClass(objectRef.get)
+        val maybeClazz = (clazz :: clazz.superClasses).find(_.methods.exists(matches))
+        if (maybeClazz.isDefined) {
+          maybeClazz.get.lookupMethod(i.getMethodName(pool)).get
+        } else {
+          clazz.interfaces
+            .find(_.methods.exists(m => !m.isAbstract && matches(m)))
+            .flatMap(_.lookupMethod(i.getMethodName(pool)))
+            .get
+        }
+      case _: INVOKESPECIAL =>
+        val clazz = Program.getClass(i.getClassName(pool))
+        val maybeClazz = (clazz :: clazz.superClasses).find(_.methods.exists(m =>
+          m.isInstanceMethod && matches(m)))
+        if (maybeClazz.isDefined) {
+          maybeClazz.get.lookupMethod(i.getMethodName(pool)).get
+        } else {
+          val objectClass = Program.getClass(classOf[java.lang.Object].getName)
+          if (clazz.isInterface && objectClass.methods.exists(m => m.isPublic && matches(m))) {
+            objectClass.methods.find(m => m.isPublic && matches(m)).get
+          } else {
+            clazz.interfaces
+              .find(_.methods.exists(m => !m.isAbstract && matches(m)))
+              .flatMap(c => c.lookupMethod(i.getMethodName(pool)))
+              .get
+          }
+        }
+      case _: INVOKESTATIC =>
+        Program.getClass(i.getClassName(pool)).lookupMethod(i.getMethodName(pool)).get
+      case _: INVOKEVIRTUAL =>
+        val clazz      = Program.getClass(objectRef.get)
+        val maybeClazz = (clazz :: clazz.superClasses).find(_.methods.exists(matches))
+        if (maybeClazz.isDefined) {
+          maybeClazz.get.lookupMethod(i.getMethodName(pool)).get
+        } else {
+          clazz.interfaces
+            .find(_.methods.exists(m => !m.isAbstract && matches(m)))
+            .flatMap(c => c.lookupMethod(i.getMethodName(pool)))
+            .get
+        }
+    }
+  }
+
+  def calculateSummary(methodToInvoke: Method, frame: Frame): ConnectionGraph = {
+    val Frame(currentMethod, _, _, _, _, summaryCache) = frame
 
     // get all successors recursive
     val recursiveSuccessors = methodToInvoke.callGraph.recursive()
@@ -30,10 +130,13 @@ private[interpreter] object InvokeInterpreter extends InterpreterBuilder[InvokeI
     // check if call of 'methodToInvoke' causes 'currentMethod' to be called -> endless recursion!
     val isRecursive = recursiveSuccessors.contains(currentMethod)
 
-    if (!isRecursive) {
+    if (!methodToInvoke.isDefined) {
+      ConnectionGraph.empty()
+    } else if (!isRecursive) {
       methodToInvoke.summary
     } else {
-      val cgBefore = summaryCache.getOrElse(currentMethod, currentMethod.nonRecursiveSummary)
+      val cgBefore =
+        summaryCache.getOrElse(currentMethod, frame.cg.merge(currentMethod.nonRecursiveSummary))
       val cgMethodToInvoke =
         summaryCache.getOrElse(methodToInvoke, methodToInvoke.nonRecursiveSummary)
       val cgAfter = cgBefore.merge(cgMethodToInvoke)
@@ -43,10 +146,10 @@ private[interpreter] object InvokeInterpreter extends InterpreterBuilder[InvokeI
       } else {
         val initialFrame =
           Frame(methodToInvoke).copy(summaryCache = summaryCache + (currentMethod -> cgAfter))
-        IntraproceduralAnalysis(initialFrame,
-                                methodToInvoke.controlFlowGraph,
-                                findSuccessors = block => block.successors,
-                                findPredecessors = block => block.predecessors)
+        SummaryInformation(initialFrame,
+                           methodToInvoke.controlFlowGraph,
+                           findSuccessors = block => block.successors,
+                           findPredecessors = block => block.predecessors)
       }
     }
   }
